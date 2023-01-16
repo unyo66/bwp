032693983191501300d6ce50b45741906f404245
gohun <lljjll1235@gmail.com>
2023-01-16 22:48:11 +0900

    .

---
+++ app/src/main/generated/com/bwp/app/domain/QCompany.java
@@ -0,0 +1,53 @@
+package com.bwp.app.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QCompany is a Querydsl query type for Company
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QCompany extends EntityPathBase<Company> {
+
+    private static final long serialVersionUID = 1857519040L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QCompany company = new QCompany("company");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath name = createString("name");
+
+    public final QUserAccount userAccount;
+
+    public QCompany(String variable) {
+        this(Company.class, forVariable(variable), INITS);
+    }
+
+    public QCompany(Path<? extends Company> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QCompany(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QCompany(PathMetadata metadata, PathInits inits) {
+        this(Company.class, metadata, inits);
+    }
+
+    public QCompany(Class<? extends Company> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.userAccount = inits.isInitialized("userAccount") ? new QUserAccount(forProperty("userAccount")) : null;
+    }
+
+}
+
